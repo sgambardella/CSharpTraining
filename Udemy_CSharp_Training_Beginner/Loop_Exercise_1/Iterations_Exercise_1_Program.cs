097cf9111942394bb47b29b0694505ad8e8e9b9d
Shaun Gambardella <shaun_gambardella@gmail.com>
2020-08-04 16:45:10 -0700

    Iteration Training
    Added Exercise 1 of the Iteration Training

---
+++ Udemy_CSharp_Training_Beginner/Loop_Exercise_1/Iterations_Exercise_1_Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Exercise_1
+{
+    class Iterations_Exercise_1_Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 20; i >= 1; i--)
+            {
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
